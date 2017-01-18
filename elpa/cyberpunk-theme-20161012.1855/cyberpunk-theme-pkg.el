3369180e1ad7b68df128379b95c9414495d20d83
Donnie Adams <donald.g.adams@me.com>
2017-01-18 15:34:39 -0700

    Removed elpa directory

--- elpa/cyberpunk-theme-20161012.1855/cyberpunk-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cyberpunk-theme" "20161012.1855" "Cyberpunk Color Theme" 'nil :keywords '("color" "theme" "cyberpunk"))
--- elpa/osx-clipboard-20141012.17/osx-clipboard-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "osx-clipboard" "20141012.17" "Use the OS X clipboard from terminal Emacs" 'nil :url "https://github.com/joddie/osx-clipboard-mode")
